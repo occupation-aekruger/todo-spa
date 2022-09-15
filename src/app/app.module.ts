27deda148a1b5f699e0fcdf85e4a4b4af65df654
Anthony-Procentrica <krugera@capricorn.com.na>
2022-09-15 23:14:26 +0200

    base components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PageHeaderComponent } from './page-header/page-header.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHeaderComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule
